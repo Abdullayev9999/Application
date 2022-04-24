1e89cd5cd31e3e26b41a2f10a7f9de71a93117b0
Abdullayev9999 <104319761+Abdullayev9999@users.noreply.github.com>
2022-04-24 22:22:40 +0400

    Add files via upload

---
+++ SimpleForm/src/main/java/com/project/simpleform/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.project.simpleform.repository;
+
+import com.project.simpleform.model.UsersModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<UsersModel, Integer> {
+    Optional<UsersModel> findByLoginAndPassword(String login, String password);
+}
